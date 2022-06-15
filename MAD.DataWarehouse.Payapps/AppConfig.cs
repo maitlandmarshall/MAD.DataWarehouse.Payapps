e3cdee00c61e48fb7082ada95b34feb05c994a27
maitlandmarshall <maitlandmarshallapps@gmail.com>
2022-06-15 12:07:48 +1000

    Add project files.

---
+++ MAD.DataWarehouse.Payapps/AppConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAD.DataWarehouse.Payapps
+{
+    public class AppConfig
+    {
+        public string ConnectionString { get; set; }
+    }
+}
